1d4d609f425d5c9da93ad2c2377cf35b55efb8c8
m.hegazi <crouz221f@yahoo.com>
2024-06-05 16:56:31 +0200

    BeerDTO adding Lombok properties closes #1 , #2

--- src/main/java/com/midohegazi/mido_beer_service/web/model/BeerDTO.java
+++ src/main/java/com/midohegazi/mido_beer_service/web/model/BeerDTO.java
@@ -1,9 +1,17 @@
 package com.midohegazi.mido_beer_service.web.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class BeerDTO {
 
     private UUID id;
